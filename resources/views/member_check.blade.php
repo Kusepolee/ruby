adf4e66684809c8862831b0e2d74741751d2d264
Kris <me@restrose.net>
2016-07-22 08:48:06 +0800

    init
    init

--- resources/views/member_check.blade.php
+++ resources/views/member_check.blade.php
@@ -52,8 +52,7 @@ Session::has('deviceid') ? $deviceid = Session::get('deviceid') : $deviceid = 'N
     function locate()
     {
     	wx.getLocation({
-            //type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
-		    type: 'gcj02',
+            type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
 		    success: function (res) {
 		        $("#latitude").val(res.latitude); // 纬度，浮点数，范围为90 ~ -90
 		        $("#longitude").val(res.longitude ); // 经度，浮点数，范围为180 ~ -180。
