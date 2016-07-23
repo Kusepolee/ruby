1f7f316b91787ed7484dbc4395e349b3ab70a7e4
Kris <me@restrose.net>
2016-07-23 15:24:07 +0800

    init
    init

--- resources/views/panel/config.blade.php
+++ resources/views/panel/config.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
 <div class="container">
-
+    <ol class="breadcrumb">
+	    <li><button type="button" class="btn btn-xs btn-info">返回工作面版</button></li>
+    </ol>
 </div>
 @endsection
