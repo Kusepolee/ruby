212b762b2c35a6e339c6a7b48123ffde81b67dfa
Kris <me@restrose.net>
2016-07-31 19:45:14 +0800

    init
    init

--- socket.js
+++ socket.js
@@ -1,3 +1,6 @@
+// https server ssl服务器
+// ---------------------
+
 var fs = require('fs');
 var app = require('express')();
 var options = {
@@ -20,23 +23,15 @@ https.listen(3000, function(){
     console.log('Listening on Port 3000');
 });
 
-// var fs = require('fs');
-// var https = require('https');
+//http 服务器
+//-----------
 
-// var express = require('express');
-// var app = express();
-
-// var options = {
-//   key: fs.readFileSync('/etc/letsencrypt/live/henjou.com/privkey.pem'),
-//   cert: fs.readFileSync('/etc/letsencrypt/live/henjou.com/fullchain.pem')
-// };
-// // var serverPort = 443;
-// var server = https.createServer(options, app);
-// var io = require('socket.io')(server);
-
+// var app = require('express')();
+// var http = require('http').Server(app);
+// var io = require('socket.io')(http);
 // var Redis = require('ioredis');
 // var redis = new Redis();
-// redis.subscribe('message-channel', function(err, count) {
+// redis.subscribe('test-channel', function(err, count) {
 // });
 // redis.on('message', function(channel, message) {
 //     console.log('Message Recieved: ' + message);
@@ -45,17 +40,4 @@ https.listen(3000, function(){
 // });
 // http.listen(3000, function(){
 //     console.log('Listening on Port 3000');
-// });
-
-// app.get('/', function(req, res) {
-//   res.sendFile(__dirname + '/public/index.html');
-// });
-
-// io.on('connection', function(socket) {
-//   console.log('new connection');
-//   socket.emit('message', 'This is a message from the dark side.');
-// });
-
-// server.listen(serverPort, function() {
-//   console.log('server up and running at %s port', serverPort);
 // });
