6f6f83e7a221075549473fb27d4d94c51e615e25
Kris <me@restrose.net>
2016-07-31 19:33:04 +0800

    init
    init

--- socket.js
+++ socket.js
@@ -1,11 +1,38 @@
+// var fs = require('fs');
+// var app = require('express')();
+// var http = require('https').Server(app);
+// var options = {
+//   key: fs.readFileSync('/etc/letsencrypt/live/henjou.com/privkey.pem'),
+//   cert: fs.readFileSync('/etc/letsencrypt/live/henjou.com/fullchain.pem')
+// };
+// var io = require('socket.io')(http);
+// var Redis = require('ioredis');
+// var redis = new Redis();
+// redis.subscribe('message-channel', function(err, count) {
+// });
+// redis.on('message', function(channel, message) {
+//     console.log('Message Recieved: ' + message);
+//     message = JSON.parse(message);
+//     io.emit(channel + ':' + message.event, message.data);
+// });
+// http.listen(3000, function(){
+//     console.log('Listening on Port 3000');
+// });
+
 var fs = require('fs');
-var app = require('express')();
-var http = require('https').Server(app);
+var https = require('https');
+
+var express = require('express');
+var app = express();
+
 var options = {
   key: fs.readFileSync('/etc/letsencrypt/live/henjou.com/privkey.pem'),
   cert: fs.readFileSync('/etc/letsencrypt/live/henjou.com/fullchain.pem')
 };
-var io = require('socket.io')(http);
+// var serverPort = 443;
+var server = https.createServer(options, app);
+var io = require('socket.io')(server);
+
 var Redis = require('ioredis');
 var redis = new Redis();
 redis.subscribe('message-channel', function(err, count) {
@@ -15,24 +42,9 @@ redis.on('message', function(channel, message) {
     message = JSON.parse(message);
     io.emit(channel + ':' + message.event, message.data);
 });
-http.listen(3000, function(){
+https.listen(3000, function(){
     console.log('Listening on Port 3000');
 });
-
-// var fs = require('fs');
-// var https = require('https');
-
-// var express = require('express');
-// var app = express();
-
-// var options = {
-//   key: fs.readFileSync('/etc/letsencrypt/live/henjou.com/privkey.pem'),
-//   cert: fs.readFileSync('/etc/letsencrypt/live/henjou.com/fullchain.pem')
-// };
-// var serverPort = 443;
-
-// var server = https.createServer(options, app);
-// var io = require('socket.io')(server);
 
 // app.get('/', function(req, res) {
 //   res.sendFile(__dirname + '/public/index.html');
