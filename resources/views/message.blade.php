9b8328894edd183d6e84917cd6f5739674f92c07
Kris <me@restrose.net>
2016-07-31 10:26:22 +0800

    init
    init

--- resources/views/message.blade.php
+++ resources/views/message.blade.php
@@ -1,3 +1,7 @@
+<?php
+$h = new FooWeChat\Helpers\Helper;
+$socket = $h->app('ssl').'://'.$h->custom('url').':3000';
+?>
 @extends('head')
 
 @section('content')
@@ -6,7 +10,7 @@
 <script src="https://cdn.socket.io/socket.io-1.3.5.js"></script>
 <script>
         //var socket = io('http://localhost:3000');
-        var socket = io('http://127.0.0.1:3000');
+        var socket = io('<?php echo $socket; ?>');
         socket.on("message-channel:App\\Events\\MessageEvent", function(message){
             // increase the power everytime we load test route
             $('#power').text(parseInt($('#power').text()) + parseInt(message.data.power));
